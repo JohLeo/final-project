7972bfcadd59028fbc8a1aac0648e42ef8449773
Johanna Leonsson <johanna@leonsson.se>
2023-06-06 19:19:22 +0200

    solved conflict with renamed component from mater

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import { Nav } from 'components/nav';
+import { Nav } from 'components/Navigation';
 import { ContainerCenter } from 'components/GlobalStyles'
 
 export const IntroPage = () => {
