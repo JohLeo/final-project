fc008ae2ae09b147f3b2aa8883fbe962182473fb
Hannah Ek <ekhannahek@gmail.com>
2023-06-11 08:34:47 +0200

    changed category into a dropdown menu

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,28 +1,62 @@
 /* eslint-disable max-len */
-// IMPORT MODULES
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import Button from './lib/Button'
-
-// COMPONENT
+import { Link } from 'react-router-dom';
+import Button from './lib/Button';
 
 export const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const [minSquareMeters, setMinSquareMeters] = useState('');
+  const [maxSquareMeters, setMaxSquareMeters] = useState('');
+  const [category, setCategory] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+
+  const categoryOptions = ['Apartment', 'House', 'Vacation Home'];
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
+  const handleMinPriceChange = (event) => {
+    setMinPrice(event.target.value);
+  };
+
+  const handleMaxPriceChange = (event) => {
+    setMaxPrice(event.target.value);
+  };
+
+  const handleMinSquareMetersChange = (event) => {
+    setMinSquareMeters(event.target.value);
+  };
+
+  const handleMaxSquareMetersChange = (event) => {
+    setMaxSquareMeters(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
   const handleSearchSubmit = (event) => {
     event.preventDefault();
 
-    // PERFORM THE SEARCH WITH THE SEARCHTERM
-    console.log('Performing search with:', searchTerm);
-    fetch(`https://final-project-backend-4l5tpsxxuq-ew.a.run.app/properties?location=${searchTerm}`)
+    // Build the query string based on the search parameters
+    const queryString = new URLSearchParams({
+      location: searchTerm,
+      minPrice,
+      maxPrice,
+      minSquareMeters,
+      maxSquareMeters,
+      type: category
+    }).toString();
+
+    // Perform the search with the query string
+    fetch(`https://final-project-backend-4l5tpsxxuq-ew.a.run.app/properties?${queryString}`)
       .then((response) => response.json())
       .then((data) => {
-      // PROCESS THE SEARCH RESULTS
+        // Process the search results
         console.log('Search results:', data);
         const modifiedData = data.map((result) => ({
           id: result.id,
@@ -50,12 +84,40 @@ export const Search = () => {
       <Form onSubmit={handleSearchSubmit}>
         <Input
           type="text"
-          placeholder="Search..."
+          placeholder="City"
           value={searchTerm}
           onChange={handleSearchChange} />
-        <Button
-          type="submit"
-          text="Search" />
+        <Select
+          value={category}
+          onChange={handleCategoryChange}>
+          <option value="">Select category</option>
+          {categoryOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </Select>
+        <Input
+          type="number"
+          placeholder="Min Price"
+          value={minPrice}
+          onChange={handleMinPriceChange} />
+        <Input
+          type="number"
+          placeholder="Max Price"
+          value={maxPrice}
+          onChange={handleMaxPriceChange} />
+        <Input
+          type="number"
+          placeholder="Min Square Meters"
+          value={minSquareMeters}
+          onChange={handleMinSquareMetersChange} />
+        <Input
+          type="number"
+          placeholder="Max Square Meters"
+          value={maxSquareMeters}
+          onChange={handleMaxSquareMetersChange} />
+        <Button type="submit" text="Search" />
       </Form>
 
       {/* Display search results */}
@@ -65,12 +127,15 @@ export const Search = () => {
           <ul>
             {searchResults.map((result) => (
               <ResultItem key={result.id}>
-                <Title>Description:</Title> {result.description}<br />
-                <Title>Category:</Title> {result.category}<br />
-                <Title>Street address:</Title> {result.address.street} {result.address.streetNumber}<br /><br />
-                <Title>Price:</Title> {result.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} {result.currency}<br />
-                <Title>Size:</Title> {result.squareMeters} {result.unitOfArea}<br />
-                <Title>Realtor:</Title> {result.realtor}<br />
+                <Link to={`/properties/${result.id}`}>
+                  {/* Update the URL to include the id */}
+                  <Title>Description:</Title> {result.description}<br />
+                  <Title>Category:</Title> {result.category}<br />
+                  <Title>Street address:</Title> {result.address.street} {result.address.streetNumber}<br /><br />
+                  <Title>Price:</Title> {result.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} {result.currency}<br />
+                  <Title>Size:</Title> {result.squareMeters} {result.unitOfArea}<br />
+                  <Title>Realtor:</Title> {result.realtor}<br />
+                </Link>
               </ResultItem>
             ))}
           </ul>
@@ -83,42 +148,49 @@ export const Search = () => {
 // STYLING
 
 const Form = styled.form`
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-`;
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+  `;
 
 const Input = styled.input`
+    padding: 8px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  `;
+
+const ResultsContainer = styled.div`
+    margin-top: 20px;
+  
+    h2 {
+      margin-bottom: 10px;
+    }
+  
+    ul {
+      list-style: none;
+      padding: 0;
+    }
+  
+    li {
+      margin-bottom: 5px;
+    }
+  `;
+
+const ResultItem = styled.li`
+    margin-bottom: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  `;
+
+const Select = styled.select`
   padding: 8px;
   font-size: 16px;
   border: 1px solid #ccc;
   border-radius: 4px;
 `;
 
-const ResultsContainer = styled.div`
-  margin-top: 20px;
-
-  h2 {
-    margin-bottom: 10px;
-  }
-
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-
-  li {
-    margin-bottom: 5px;
-  }
-`;
-
-const ResultItem = styled.li`
-  margin-bottom: 10px;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-`;
-
 const Title = styled.strong`
-  font-weight: bold;
-`;
+    font-weight: bold;
+  `;
