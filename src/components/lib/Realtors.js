8e2843ccf98a282ab4471baececdc7ad3421762e
Johanna <120407185+JohLeo@users.noreply.github.com>
2023-06-14 14:16:04 +0200

    better structure for contact and added agent info to object + headline (#39)

--- src/components/lib/Realtors.js
+++ src/components/lib/Realtors.js
@@ -4,31 +4,70 @@ import styled from 'styled-components';
 const RealtorContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  text-align: center;
+  align-items: left;
+  text-align: left;
 `;
 
 const RealtorImage = styled.img`
-  max-width: 100%;
+  max-width: 150px;
   height: auto;
   border-style: solid;
   border-width: 5px 5px 14px 5px; 
   border-color: #E6E6E8;
-  margin: 10px 10px 0;
+  margin: 10px 0;
 `;
 
-const RealtorName = styled.p`
-  color: #414142;
-  font-size: 11px;
-  font-weight: 300;
-  margin: 8px 0;
-`;
-
-export const Realtor = ({ src, alt, ariaLabel, name }) => {
+export const Realtor = ({ src, alt, ariaLabel }) => {
   return (
     <RealtorContainer>
-      <RealtorImage src={src} alt={alt} aria-label={ariaLabel} />
-      <RealtorName>{name}</RealtorName>
+      <RealtorImage
+        src={src}
+        alt={alt}
+        aria-label={ariaLabel} />
     </RealtorContainer>
+  );
+};
+
+export const Title = styled.p`
+ margin: 0;
+ color: var(--text);
+ font-size: 10px;
+ font-weight: 300;
+`
+export const Name = styled.p`
+ margin: 0;
+ color: var(--text);
+ font-size: 14px;
+ font-weight: 400;
+ text-transform: uppercase;
+`
+export const Phone = styled.a`
+ margin: 10px 0 0;
+ color: var(--text);
+ font-size: 14px;
+ font-weight: 700;
+ text-decoration: none;
+`
+export const Email = styled.a`
+ margin: 0;
+ color: var(--text);
+ font-size: 10px;
+ font-weight: 500;
+ text-decoration: none;
+`
+export const UsDetails = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+`
+
+export const ContactInfo = ({ title, name, phone, email }) => {
+  return (
+    <UsDetails>
+      <Title>{title}</Title>
+      <Name>{name}</Name>
+      <Phone href={`tel:${phone}`}>{phone}</Phone>
+      <Email href={`mailto:${email}`}>{email}</Email>
+    </UsDetails>
   );
 };
