92004c28eff8ed6962f2f24bf2670e8c5174450b
Hannah Ek <ekhannahek@gmail.com>
2023-06-15 14:43:06 +0200

    smashed bug that appeared after styling of menu folding. side scroll appeared (#51)

--- src/components/Menu/Menu.styled.js
+++ src/components/Menu/Menu.styled.js
@@ -19,22 +19,20 @@ const slideOut = keyframes`
 `;
 
 export const StyledMenu = styled.nav`
-  display: flex;
+  display: ${({ open }) => (open ? 'flex' : 'none')};
   flex-direction: column;
   justify-content: center;
   background: white;
   text-align: left;
   height: 100vh;
   padding: 2rem;
-  position: absolute;
+  position: fixed;
   top: 0;
-  right: 0;
-  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
-  transition: transform 0.3s ease-in-out;
+  right: ${({ open }) => (open ? '0' : '-100%')}; 
+  transition: right 0.3s ease-in-out; 
   overflow: hidden;
   z-index: 999;
   animation: ${({ open }) => (open ? slideIn : slideOut)} 0.3s ease-in-out;
-
 
   @media (max-width: 768px) {
     width: 100%;
